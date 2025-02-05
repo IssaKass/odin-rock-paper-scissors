26b308a5ef87011daf87bb89c42f4b73d184c966
Abdallah-Issa Kass <issakass02@gmail.com>
2025-02-05 18:10:25 +0200

    Create startup and write `Understanding problem`
    - Created the startup code for HTML and JavaScript,
      and linked them together.
    - Wrote the 1st step of Problem Solving: 'Understanding the problem'.

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+  ---- Understanding the problem ----
+  - The game will be played against the computer. 
+  - The computer will randomly choose between 'rock', 'paper' or 'scissors'.
+  - The player will input their choice.
+  - The game will compare both choices and determine the winner based on the rules:
+    - Rock beats Scissors
+    - Scissors beats Paper
+    - Paper beats Rock
+    - If both choices are the same, it's a tie.
+  - The game will keep track of both the player’s score and the computer’s score.
+	- The game will be played round by round, and the winner of each round will 
+  - have their score incremented.
+	- Each round’s result will be displayed with a winner announcement.
+*/
